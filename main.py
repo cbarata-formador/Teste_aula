49e5fe85401de929bab2a806cb5e233d388fdc56
Jessica <jessica.d.costa@hotmail.com>
2025-07-22 12:32:38 +0100

    added idade request

--- main.py
+++ main.py
@@ -8,3 +8,12 @@ print("Olá turma do Python!")
 print("Tudo bem!")
 nome: str = input("Qual é o seu nome? ")
 print(f"Olá, {nome}!")
+
+idade = int(input("\nQual a sua idade? "))
+
+if idade < 18:
+    print("Você é menor de idade\n")
+elif idade >= 18:
+    print("Você é maior de idade\n")
+else:
+    print("\nErro, opção inválida! ")     
